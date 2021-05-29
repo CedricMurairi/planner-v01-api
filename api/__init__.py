0176279cfb79d82f1e262b0feb32e732a38a16ec
Cedric Murairi <murairicedric@gmail.com>
2021-05-29 21:58:24 +0200

    added custom error endpoints :wrench:

--- api/__init__.py
+++ api/__init__.py
@@ -16,16 +16,46 @@ def create_app(config_name):
     CORS(app)
 
     from .user import user
-    # from .project import project
-    # from .task import task
+    from .project import project
+    from .task import task
 
     app.register_blueprint(user)
-    # app.register_blueprint(project)
-    # app.register_blueprint(task)
+    app.register_blueprint(project)
+    app.register_blueprint(task)
     
     @app.route('/welcome')
     def home():
         return "<h1>Hello everyone, welcome to the next big thing</h1>"
+
+    @app.errorhandler(404)
+    def not_fount(err):
+        return {
+            "message": "Not Found"
+        }, 404
+
+    @app.errorhandler(500)
+    def not_fount(err):
+        return {
+            "message": "Internal Server error"
+        }, 500
+
+    @app.errorhandler(400)
+    def not_fount(err):
+        return {
+            "message": "Bad Request"
+        }, 400
+
+    @app.errorhandler(405)
+    def not_fount(err):
+        return {
+            "message": "Method not allowed"
+        }, 405
+
+    @app.errorhandler(403)
+    def not_fount(err):
+        return {
+            "message": "Forbidden"
+        }, 403
 
     return app
 
